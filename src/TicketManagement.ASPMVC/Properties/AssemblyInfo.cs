3740149111929b1d06d8ef7bcaf1b00d2296dd28
Gumar Diana <lantan.mp4@gmail.com>
2020-10-16 12:44:00 +0300

    add asp mvc (to check reference truble)

--- src/TicketManagement.ASPMVC/Properties/AssemblyInfo.cs
+++ src/TicketManagement.ASPMVC/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TicketManagement.DomainModel")]
+[assembly: AssemblyTitle("TicketManagement.ASPMVC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TicketManagement.DomainModel")]
+[assembly: AssemblyProduct("TicketManagement.ASPMVC")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("06862518-364c-4f34-aa26-5cdfbfd0dde8")]
+[assembly: Guid("25816e36-3012-4293-89f7-109a4698f227")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
