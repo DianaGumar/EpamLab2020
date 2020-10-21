b5038d01381f45fe51cc678d2b481d7c4e838ea9
Gumar Diana <lantan.mp4@gmail.com>
2020-10-21 12:11:09 +0300

    add domain model proj

---
+++ src/TicketManagement.Web/Controllers/TMEventController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace TicketManagement.Web.Controllers
+{
+    public class TMEventController : Controller
+    {
+        // GET: TMEvent
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
