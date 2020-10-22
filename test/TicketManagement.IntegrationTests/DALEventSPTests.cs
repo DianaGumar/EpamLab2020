1b7045590f4acbe0cf54ae33c20e56f7636b8a2f
Gumar Diana <lantan.mp4@gmail.com>
2020-10-22 08:36:56 +0300

    some experiense with publish db programarly

--- test/TicketManagement.IntegrationTests/DALEventSPTests.cs
+++ test/TicketManagement.IntegrationTests/DALEventSPTests.cs
@@ -1,23 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using FluentAssertions;
+using Microsoft.Build.Execution;
+using Microsoft.Build.Framework;
+using Microsoft.SqlServer.Dac;
 using NUnit.Framework;
 using TicketManagement.DataAccess.DAL;
 using TicketManagement.DataAccess.Model;
+using ILogger = Microsoft.Build.Framework.ILogger;
 
 namespace TicketManagement.IntegrationTests
 {
     public class DALEventSPTests
     {
+        private static TextWriter _output = new StreamWriter("output.txt", false);
+
         private readonly string _str = ConfigurationManager.ConnectionStrings["LocalSqlServer"].ConnectionString;
+
+        private bool CreateDataBase()
+        {
+            var props = new Dictionary<string, string>();
+            props.Add("UpdateDatabase", "True");
+            props.Add("SqlPublishProfilePath", "TicketManagement.Database/TicketManagement.publish.xml");
+
+            var projPath = "TicketManagement.Database/TicketManagement.Database.sqlproj";
+
+            var result = BuildManager.DefaultBuildManager.Build(
+                new BuildParameters { Loggers = new[] { new ConsoleLogger() } },
+                new BuildRequestData(new ProjectInstance(projPath, props, null), new[] { "Publish" }));
+
+            if (result.OverallResult == BuildResultCode.Success)
+            {
+                return true;
+            }
+
+            Create();
+
+            return false;
+        }
+
+        private void Create()
+        {
+            // Class responsible for the deployment. (Connection string supplied by console input for now)
+            DacServices dbServices = new DacServices(_str);
+
+            // Wire up events for Deploy messages and for task progress
+            // (For less verbose output, don't subscribe to Message Event (handy for debugging perhaps?)
+            dbServices.Message += new EventHandler<DacMessageEventArgs>(DbServices_Message);
+            dbServices.ProgressChanged += new EventHandler<DacProgressEventArgs>(DbServices_ProgressChanged);
+
+            // This Snapshot should be created by our build process using MSDeploy
+            DacPackage dbPackage = DacPackage.Load(Console.ReadLine());
+
+            DacDeployOptions dbDeployOptions = new DacDeployOptions();
+
+            // Cut out a lot of options here for configuring deployment, but are all part of DacDeployOptions
+            dbDeployOptions.SqlCommandVariableValues.Add("debug", "false");
+
+            dbServices.Deploy(dbPackage, "trunk", true, dbDeployOptions);
+            _output.Close();
+        }
+
+        private void DbServices_Message(object sender, DacMessageEventArgs e)
+        {
+            _output.WriteLine("DAC Message: {0}", e.Message);
+        }
+
+        private void DbServices_ProgressChanged(object sender, DacProgressEventArgs e)
+        {
+            _output.WriteLine(e.Status + ": " + e.Message);
+        }
 
         // Same test data are located
         // TicketManagement.Database/Post/Script.TestEventSPData.sql
         [Test]
         public void CreateEventTest()
         {
+            if (!CreateDataBase())
+            {
+                 Assert.Fail();
+            }
+
             // create tested data
             // arange
             IVenueRepository venueRepository = new VenueRepository(_str);
@@ -293,6 +359,35 @@ namespace TicketManagement.IntegrationTests
             tmeventseats.ForEach(ta => ta.Id = 0);
             seats2.ForEach(ta => ta.Id = 0);
             tmeventseats.Should().BeEquivalentTo(seats2, options => options.ExcludingMissingMembers());
+        }
+
+        private class ConsoleLogger : ILogger
+        {
+            public LoggerVerbosity Verbosity { get; set; }
+
+            public string Parameters { get; set; }
+
+            public void Initialize(IEventSource eventSource)
+            {
+                eventSource.ErrorRaised += (sender, e) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Message);
+                    Console.ResetColor();
+                };
+                eventSource.MessageRaised += (sender, e) =>
+                {
+                    if (e.Importance != MessageImportance.Low)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                };
+            }
+
+            public void Shutdown()
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
