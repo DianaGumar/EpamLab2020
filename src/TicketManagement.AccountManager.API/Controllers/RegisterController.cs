cee08e1230b6c8f0f87e5b70cdcb1ad8cd85bbf2
DianaGumar <lantan.mp4@gmail.com>
2021-05-04 12:28:06 +0300

    added register, login and logout options

---
+++ src/TicketManagement.AccountManager.API/Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketManagement.AccountManager.API.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RegisterController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/TicketManagement.UserManager.API/Dto/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace TicketManagement.UserManager.API.Dto
-{
-    public class User : IdentityUser
-    {
-        public int Year { get; set; }
-    }
-}
